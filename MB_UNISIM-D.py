32d727103a4b285d6863fac3a2d24d81d30408ee
marcelogarret <marcelogarret@hotmail.com>
2023-04-26 00:11:28 -0300

    Atualização do ajuste do aquífero

--- MB_UNISIM-D.py
+++ MB_UNISIM-D.py
@@ -8,21 +8,23 @@ Autor: Marcelo Garret de Melo Filho
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from imblearn.over_sampling import RandomOverSampler
-from sklearn.preprocessing import StandardScaler
-import copy
-import seaborn as sns
-import tensorflow as tf
-from sklearn.linear_model import LinearRegression
-import xgboost as xgb
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import TimeSeriesSplit
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
+# from imblearn.over_sampling import RandomOverSampler
+# from sklearn.preprocessing import StandardScaler
+# import copy
+# import seaborn as sns
+# import tensorflow as tf
+# from sklearn.linear_model import LinearRegression
+# import xgboost as xgb
+# from sklearn.metrics import mean_squared_error
+# from sklearn.model_selection import TimeSeriesSplit
 
 df=pd.read_excel('unisim_hist.xlsx')
 df=df.drop(["Press_b", "Np_b", "Gp_b", "Wp_b", "Winj_b"], axis=1)
-df.head()
+# print(df.head())
 
-print(df.dtypes)
+# print(df.dtypes)
 
 # Parametros escalares (MODSI)
 phi = 0.136
@@ -36,15 +38,15 @@ cf = 5.3E-05
 pb = 210.3
 bob = 1.414
 co = 0.000162
+cw = 48E-6
+Swi = 0.17
 
+df["Rp"] = df["Gp"]/df["Np"]
 df["t"] = (df["Date"]-df["Date"].iloc[0]).astype("int64")/10**9/60/60/24
 df["dt"]=df["t"].diff()
 df["p"] = df["Press"].iloc[0]-df["Press"]
 df["dp"]=df["p"].diff()
-
-print(df.head())
-
-df["Rp"] = df["Gp"]/df["Np"]
+# print(df.head())
 
 ## PVT functions
 
@@ -62,26 +64,27 @@ def func_rs(p, a, b):
     return np.append(rs1, rs2)
 
 p = df["Press"].values
-bo =func_bo(p, 0.0012, 1.1538)
+bo = func_bo(p, 0.0012, 1.1538)
 df["Bo"]=bo
 
-bg=func_bg(p, 1.40676, -1.04229)
+bg = func_bg(p, 1.40676, -1.04229)
 df["Bg"]=bg
 
-rs=func_rs(p, 0.4655, 15.0114)
+rs = func_rs(p, 0.4655, 15.0114)
 df["Rs"]=rs
 
 df["Bt"]=df["Bo"]+(Rsi-df["Rs"])*df["Bg"]
 df["F"] = df["Np"]*(df["Bt"]+(df["Rp"]-Rsi)*df["Bg"])+(df["Wp"]-df["Winj"])*Bw
 df["Eo"] = df["Bt"]-df["Bt"].iloc[0]
 df["Eg"] = df["Bt"].iloc[0]*(df["Bg"]/df["Bg"].iloc[0]-1)
-df["We"] = df["F"]-N*(df["Eo"]+m*df["Eg"])
-df["x"] = df["We"]/(df["Eo"]+m*df["Eg"])/1E6
-df["y"] = df["F"]/(df["Eo"]+m*df["Eg"])/1E6
+df["Efw"] = df["Bt"].iloc[0]*((cf+cw*Swi)/(1-Swi))*df["dp"]
+df["We"] = df["F"]-N*(df["Eo"]+m*df["Eg"]+(1+m)*df["Efw"])
+df["x"] = df["We"]/(df["Eo"]+m*df["Eg"]+(1+m)*df["Efw"])/1E6
+df["y"] = df["F"]/(df["Eo"]+m*df["Eg"]+(1+m)*df["Efw"])/1E6
 
 # df = df.set_index("Date")
+# df=df.dropna()
 
-df=df.dropna()
 print(df.head())
 
 plt.scatter(df["x"], df["y"])
@@ -112,34 +115,56 @@ ax.set_title('3D line plot')
 plt.show()
 
 train = df.copy()
+train = train.drop(["Np", "Gp", "Rp", "Wp", "Winj", "Bt", "Bo", "Bg", "Rs", "F", "Eo", "Eg", "Efw", "x", "y", "p", "dt"], axis=1)
 
-FEATURES_we = ["t", "p"]
-TARGET_we = "We"
+p=train["Press"].values
+we=train["We"].values
+t=train["t"].values
+we[0]=0
+we[1]=0
 
-X_train_we = train[FEATURES_we]
-y_train_we = train[TARGET_we]
+##Aquífero Schilthuis
+def func_we1(t, J):
+    dt=t-np.roll(t,1)
+    p_med=(p+np.roll(p,1))/2
+    pmt = ((p[0]-p_med[2:])*dt[2:]).cumsum()
+    return np.append(np.zeros(2), J*pmt[0:len(t)])
 
-reg_we = xgb.XGBRegressor()
-reg_we.fit(X_train_we, y_train_we)
+##Aquífero Hurst Modificado
+def func_we2(t, C, a):
+    dt=t-np.roll(t,1)
+    p_med=(p+np.roll(p,1))/2
+    pmt = (((p[0]-p_med[1:])*dt[1:])/np.log(a*t[1:])).cumsum()
+    return np.append(np.zeros(1), C*pmt[0:len(t)])
 
-print(reg_we.score(X_train_we, y_train_we))
-print(reg_we.predict(X_train_we))
+print(train.head())
 
-df["We_Pred"] = reg_we.predict(X_train_we)
+# initialGuess=[1]
+initialGuess=[1,1]
+# popt,pcov = curve_fit(func_we1, t, we, initialGuess)
+popt,pcov = curve_fit(func_we2, t, we, initialGuess)
+print(popt)
 
-plt.scatter(df["t"], df["We"], label="Hist", color="blue")
-plt.plot(df["t"], df["We_Pred"], label="Fit", color="red", linewidth=3)
-plt.title("We x t")
+# fittedData=func_we1(t, *popt)
+fittedData=func_we2(t, *popt)
+r2 = r2_score(we, fittedData)
+print(r2)
+
+train["We_pred"]=fittedData
+
+plt.scatter(t, we, label="Data", color="blue")
+# plt.plot(t, fittedData, label=f"Fit: J = {popt[0]:0.2f} | R\N{SUPERSCRIPT TWO} = {r2:.2f}", color="red", linewidth=3)
+plt.plot(t, fittedData, label=f"Fit: C = {popt[0]:0.2f} ; a = {popt[1]:0.3f} | R\N{SUPERSCRIPT TWO} = {r2:.2f}", color="red", linewidth=3)
 plt.legend()
-plt.xlabel("dt")
+plt.xlabel("t")
 plt.ylabel("We")
 plt.show()
 
-plt.scatter(df["p"], df["We"], label="Hist", color="blue")
-plt.plot(df["p"], df["We_Pred"], label="Fit", color="red", linewidth=3)
-plt.title("We x p")
+plt.scatter(p, we, label="Data", color="blue")
+# plt.plot(p, fittedData, label=f"Fit: J = {popt[0]:0.2f} | R\N{SUPERSCRIPT TWO} = {r2:.2f}", color="red", linewidth=3)
+plt.plot(p, fittedData, label=f"Fit: C = {popt[0]:0.2f} ; a = {popt[1]:0.3f} | R\N{SUPERSCRIPT TWO} = {r2:.2f}", color="red", linewidth=3)
 plt.legend()
-plt.xlabel("dp")
+plt.xlabel("p")
 plt.ylabel("We")
 plt.show()
 
@@ -148,34 +173,8 @@ ax = plt.axes(projection ='3d')
  
 # plotting
 ax.scatter(df["t"], df["p"], df["We"])
-ax.plot3D(df["t"], df["p"], df["We_Pred"], 'green')
+ax.plot3D(df["t"], df["p"], train["We_pred"], 'green')
 ax.set_title('3D line plot')
 plt.show()
 
-train=df.copy()
-
-FEATURES_press= ["F", "Eo", "Eg", "We_Pred"]
-TARGET_press = "Press"
-
-X_train_press = train[FEATURES_press]
-y_train_press = train[TARGET_press]
-
-reg_press = xgb.XGBRegressor()
-reg_press.fit(X_train_press, y_train_press)
-
-print(reg_press.score(X_train_press, y_train_press))
-print(reg_press.predict(X_train_press))
-
-df["Press_Pred"] = reg_press.predict(X_train_press)
-
-plt.scatter(df["t"], df["Press"], label="Hist", color="blue")
-plt.plot(df["t"], df["Press_Pred"], label="Fit", color="red", linewidth=3)
-plt.title("Pressure x t")
-plt.legend()
-plt.xlabel("t")
-plt.ylabel("Pressure")
-plt.show()
-
-reg_we.save_model('unisim-d_we.json')
-reg_press.save_model('unisim-d_press.json')
-df.to_excel('unisim_hist_match.xlsx', index=True)
+train.to_excel('unisim_hist_match.xlsx', index=True)
